134e7383f968877cf0c8f3695a7add10dbec290f
yvesbastos <raphael.bastos@me.com>
2014-11-02 22:32:12 -0300

    .

---
+++ src/br/ufal/ic/ia/CheckerApplet.java
@@ -0,0 +1,16 @@
+package br.ufal.ic.ia;
+
+import java.applet.Applet;
+import java.awt.Color;
+
+public class CheckerApplet extends Applet {
+	public static int rows = 8;
+	public static int colums = 8;
+	public static Color col1=Color.black;
+	public static Color col2=Color.white;
+	
+	//https://www.youtube.com/watch?v=FKJxPlWQp9Y
+	public void init() {
+		
+	}
+}
